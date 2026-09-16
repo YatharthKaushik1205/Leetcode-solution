41e1edc152c4e0dd0982c7605393a866ec80ba15
YatharthKaushik1205 <kaushikyatharth45@gmail.com>
2026-09-16 15:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (19.89%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
         answer = []
+        maximum = max(candies)
         for i in candies:
-            if i+extraCandies >= max(candies):
+            if i+extraCandies >= maximum:
                 answer.append(True)
             else:
                 answer.append(False)
